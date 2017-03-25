b0f2892c11cb32c16de2827d540b5135415dc72e
thomas_ato <twshal@gmail.com>
2017-03-25 17:35:19 -0500

    add portfolio value to portfolio page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,16 @@
 class Portfolio < ActiveRecord::Base
+	def stock_value
+		stock_val = 0
+		Position.where(portfolio_id: self.id).each do |position|
+			stock = Stock.find(position.stock_id)
+			stock_val += (position.position_amount * stock.stock_price)
+		end
+
+		return stock_val
+	end
+
+	def total_value
+		total = self.stock_value
+		return total + self.portfolio_cash_account
+	end
 end
