811b7394cd997075c267a1cc67ea86b0cf0050c2
thomas_ato <twshal@gmail.com>
2017-03-25 19:05:36 -0500

    fix trade insertions to add trade price, date time, and position id

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -30,37 +30,87 @@ class ExchangeController < ApplicationController
 
 		Rails.logger.info(trade_params)
 
+		# Find all the records needed for this trade. 
+
+		@error = "NONE"
+
 		stock = Stock.find(params[:trade][:stock_id])
 
 		portfolio = Portfolio.find_by(user_id: session[:user_id])
 
 		trade_type = TradeType.find(params[:trade][:trade_type])
 
+		# Check if the position exists, in case we need to create a new one. 
 		if (Position.exists?(:portfolio_id => portfolio.id, :stock_id => stock.id)) then
 			position = Position.find_by(portfolio_id: portfolio.id, stock_id: stock.id)
 		elsif trade_type.trade_type_name != "SELL" then
-			position = Position.new(portfolio_id: portfolio.id, stock_id: stock.id)
+			position = Position.new(portfolio_id: portfolio.id, stock_id: stock.id, position_amount: 0)
 		else
 			# Tried to sell shares we don't have. 
+
+			@error = "Position does not exist."
+
 			respond_to do |format|
 				format.js {}
 			end
 		end
 
-		if params[:price] == false then
-			trade_params[:trade_price] = stock.stock_price
+		params[:trade][:position_id] = position.id
+		params[:trade][:trade_datetime] = DateTime.current
+		# Check if we're using market price or queueing a trade. 
+		if params[:price] == "true" then
+			params[:trade][:trade_price] = stock.stock_price
 		end
+
+
+
 		trade = Trade.new(trade_params)
+
+		# Modify the positions accordingly
+
+
 
 		if trade_type.trade_type_name == "BUY" then
 			# We need more checks here to make sure shit doesn't break
-			position.position_amount += trade_params[:trade_amount].to_d
+
+			if (stock.stock_price * trade_params[:trade_amount].to_d) < portfolio.portfolio_cash_account then
+				Rails.logger.info(portfolio.total_value)
+				Rails.logger.info(position)
+				position.position_amount += trade_params[:trade_amount].to_d
+
+				portfolio.portfolio_cash_account -= (stock.stock_price * trade_params[:trade_amount].to_d)
+				Rails.logger.info(portfolio.total_value)
+			else
+				# Can't afford the trade. what do
+				@error = "Not enough funds for this trade."
+				respond_to do |format|
+					format.js {}
+				end
+			end
+
+			
 		elsif trade_type.trade_type_name == "SELL" then
-			position.position_amount -= trade_params[:trade_amount].to_d
+
+			if (trade_params[:trade_amount].to_d <= position.position_amount) then
+				Rails.logger.info(portfolio.total_value)
+				portfolio.portfolio_cash_account += (trade_params[:trade_amount].to_d * stock.stock_price)
+				position.position_amount -= trade_params[:trade_amount].to_d
+				Rails.logger.info(portfolio.total_value)
+			else
+				# Not enough shares to sell. 
+				@error = "Not enough shares to cover this trade."
+				 respond_to do |format|
+					format.js {}
+				end
+			end
 		else
 		end
-		trade.save
-		position.save
+
+		if @error == "NONE"
+			trade.save
+			position.save
+			portfolio.save
+		end
 
 
 		respond_to do |format|
@@ -71,10 +121,12 @@ class ExchangeController < ApplicationController
 	private 
 		def trade_params
 			params.require(:trade).permit(
+				:position_id,
 				:stock_id,
 				:trade_type,
 				:trade_price,
-				:trade_amount
+				:trade_amount,
+				:trade_datetime
 			)	
 		end
 end
